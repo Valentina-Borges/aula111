95eac94e7384a3754efdf60066be71d8f5f17d25
Valentina-Borges <108351920+Valentina-Borges@users.noreply.github.com>
2023-02-16 16:08:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+/*prediction_1 = ""
+prediction_2 = ""
+
+
+WebKitCSSMatrix.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementByld("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementByld("result").innerHTML = '<img id="captured_image" src="'data__uri+'"/>';
+}};
+}
+
+console.log('m15 version:', m15.version);
+
+classifier = m15.imageClassifier('https://teachablemachine.withgoogle.com/models/J2Ip11PDF/model.json',modelLoaded');
+
+function modelLoaded() {
+    console.log('Model Loaded:');
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "A primeira previsão é " + prediction_1;
+    speak_data_2 = "A segunda previsão é " + prediction_2;
+    var utterThis = new SpeechSynthesisUtterrance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+
+*/
